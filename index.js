7a5994f443f98891b2360b6df6d285ea2d834669
tashasayzz <tashasayzz@penguin>
2025-08-30 00:24:04 +0100

    HTML. JS

---
+++ index.js
@@ -0,0 +1,31 @@
+let firstNumber = 3;
+let operator = "+";
+let secondNumber = 5;
+
+
+
+
+
+
+function add(a,b){
+return a + b 
+}
+
+function subtract(a,b){
+return a - b
+};
+
+function multiply(a, b){
+    return a * b
+};
+ function division(a, b){
+    if (b === 0) {
+        return "Error: Cannot divide by zero";
+      }
+    return a / b
+ };
+
+
+ function operate(){
+
+ }
